43c28590f3ac4d67bae43dfb85f233100d37134b
MadeInTheUSB <madeintheusb@gmail.com>
2016-04-16 23:02:06 -0400

    Update

---
+++ CS/MadeInTheUSB.Nusbio.ManagedDesktop/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MadeInTheUSB.ManagedDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butLamp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butLamp
+            // 
+            this.butLamp.Location = new System.Drawing.Point(63, 12);
+            this.butLamp.Name = "butLamp";
+            this.butLamp.Size = new System.Drawing.Size(60, 23);
+            this.butLamp.TabIndex = 0;
+            this.butLamp.Text = "Lamp Off";
+            this.butLamp.UseVisualStyleBackColor = true;
+            this.butLamp.Click += new System.EventHandler(this.butLamp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 46);
+            this.Controls.Add(this.butLamp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Nusbio - Desktop Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butLamp;
+    }
+}
+
---
+++ CS/MadeInTheUSB.Nusbio.ManagedDesktop/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MadeInTheUSB;
+using MadeInTheUSB.Adafruit;
+using MadeInTheUSB.Component;
+using MadeInTheUSB.i2c;
+using MadeInTheUSB.GPIO;
+using MadeInTheUSB.WinUtil;
+using MadeInTheUSB.Display;
+
+
+namespace MadeInTheUSB.ManagedDesktop
+{
+    public partial class Form1 : Form
+    {
+        Nusbio _nusbio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SetMessage(string m)
+        {
+            this.Text = m;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Left = Screen.PrimaryScreen.WorkingArea.Width - this.Width;
+            this.Top  = Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+
+            var serialNumber = Nusbio.Detect();
+            if (serialNumber == null) // Detect the first Nusbio available
+            {
+                this.SetMessage("Nusbio not detected");
+            }
+            else
+            {
+                // Turn all gpio off/low
+                _nusbio = new Nusbio(serialNumber);
+            }
+            this.UpdateLampButtonText();
+        }
+
+        private void butLamp_Click(object sender, EventArgs e)
+        {
+            _nusbio[NusbioGpio.Gpio0].DigitalWrite(!_nusbio[NusbioGpio.Gpio0].State);
+            this.UpdateLampButtonText();
+        }
+
+        private void UpdateLampButtonText()
+        {
+            this.butLamp.Text = string.Format("Lamp {0}", _nusbio[NusbioGpio.Gpio0].State ? "On" : "Off");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this._nusbio.Close();
+        }
+    }
+}
